a737adbc5491ff7db5d28acedfa062582eb4f177
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-08-18 00:15:56 +0530

    Video Style

--- public/script.js
+++ public/script.js
@@ -1,16 +1,21 @@
 let myVideoStream;
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+// myVideo.muted = true;
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
   .then((stream) => {
-    myVideoStream = stream;
+    console.log(stream);
+    addVideoStream(myVideo, stream);
   });
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
-  video.addEventListener("loadmetadata", () => {
+  video.addEventListener("loadedmetadata", () => {
     video.play();
   });
+  videoGrid.append(video);
 };
